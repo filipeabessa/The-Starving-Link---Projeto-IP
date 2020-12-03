b9a0aa43f69c7f6f93819b8285c4a1bf36dd8cca
AmadeoNeto <amadeotatocota@gmail.com>
2020-12-03 09:59:30 -0300

    Arrow rotation by transform

--- bullets.py
+++ bullets.py
@@ -1,14 +1,48 @@
 import pygame
+from pygame import transform
 
 class Bullets(pygame.sprite.Sprite):
     def __init__(self, x, y, speedx, speedy, image):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image).convert()
+
+        # Atribui a imagem da flecha e ajusta sua rotação
+        rotation = self.get_rotation(speedx, speedy) 
+        self.image = pygame.image.load(image).convert_alpha()
+        self.image = transform.rotate(self.image, rotation)
+
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
+
         self.speedx = speedx
         self.speedy = speedy
+
+    def get_rotation(self, speedx, speedy):
+        '''Retorna a angulação que a flecha deve receber para que seu sprite seja
+        ajustado'''
+        if speedx > 0:
+            if speedy == 0:
+                return 0
+            elif speedy > 0:
+                return 45
+            else: #speedy < 0
+                return 45
+
+        if speedy > 0:
+            if speedx == 0:
+                return 0
+            else: #speedx < 0
+                return -45
+
+        if speedx < 0:
+            if speedy == 0:
+                return 0
+            elif speedy < 0:
+                return -45
+
+        if speedy < 0:
+            return 0
+
 
     def update(self):
         self.rect.y += self.speedy
